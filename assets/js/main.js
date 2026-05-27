ae7aed81c8f346b447d4f35d98852822a5c3c0ad
Demy Adrian Moya <moyademyadrian@gmail.com>
2026-05-28 02:54:12 +0800

    Path fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,7 +233,6 @@ function renderTopbarNav() {
   const base = `${projectFolderName}/src/pages/`;
   const templates = `${projectFolderName}/src/templates/`;
   const root = `${projectFolderName}/src/`;
-  const config = `${projectFolderName}/src/config/`;
 
   const themeBtn = `
     <button class="theme-toggle" id="themeToggle" title="Toggle theme">
@@ -257,7 +256,7 @@ function renderTopbarNav() {
           <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
         </svg>
       </a>
-      <a href="${config}wishlist.php" class="topbar-icon" title="Wishlist">
+      <a href="${templates}wishlist.html" class="topbar-icon" title="Wishlist">
         <svg width="18" height="18" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
           <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
         </svg>
